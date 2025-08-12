b0273469c7356cf161116aead9847aa6c800273a
md-oliver <mdoliver43@gmail.com>
2025-08-12 16:40:18 +0200

    Add main get route, get envelope route and get envelope by id route with confirmed testing on the current data

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
 import express from "express";
+import bodyParser from "body-parser";
+import {
+  getAllFromDatabase,
+  createNewBugetEnvelope,
+  hasAnyBudgets,
+  getEnvelopeById,
+} from "./public/data.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res, next) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.get("/envelopes", (req, res, next) => {
+  if (!hasAnyBudgets()) {
+    res.send(
+      "No budgets set. Create a new envelope to help track your budgets"
+    );
+  } else {
+    res.send(getAllFromDatabase());
+  }
+});
+
+app.get("/envelopes/:id", (req, res, next) => {
+  const envelopeId = Number(req.params.id);
+
+  if (getEnvelopeById(envelopeId) !== null) {
+    const envelope = getEnvelopeById(envelopeId);
+    res.send(envelope);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server started on porrt ${PORT}`);
