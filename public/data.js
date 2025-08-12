10241fb799cc3b30f4f800768add22c52b9d77bc
md-oliver <mdoliver43@gmail.com>
2025-08-12 16:36:04 +0200

    Add data with a working data scheme and some random mock data to begin with

---
+++ public/data.js
@@ -0,0 +1,64 @@
+let idIterator = 0;
+
+const createEnvlope = (title, budget) => {
+  return {
+    id: ++idIterator,
+    title: title,
+    budget: budget,
+  };
+};
+
+let allBudgets = [];
+let defaultCategories = [
+  "groceries",
+  "fuel",
+  "healthcare",
+  "clothing",
+  "diningOut",
+  "householdItems",
+  "petCare",
+  "family",
+];
+let defaultBudgetValues = [250, 500, 750, 1000, 1500, 2000];
+
+const createTempData = () => {
+  const randomCategory = Math.floor(Math.random() * defaultCategories.length);
+  const randomBuget = Math.floor(Math.random() * defaultBudgetValues.length);
+  const envelope = createNewBugetEnvelope(
+    defaultCategories[randomCategory],
+    defaultBudgetValues[randomBuget]
+  );
+  return envelope;
+};
+
+const hasAnyBudgets = () => {
+  return allBudgets.length > 0;
+};
+
+const getAllFromDatabase = () => {
+  return allBudgets;
+};
+
+const createNewBugetEnvelope = (title, budgetAllocation) => {
+  const newEnvelope = createEnvlope(title, budgetAllocation);
+  allBudgets.push(newEnvelope);
+  return newEnvelope;
+};
+
+const getEnvelopeById = (id) => {
+  if (!isNaN(parseInt(id)) && isFinite(id)) {
+    id = Number(id);
+  } else return null;
+
+  const envelope = allBudgets.filter((budget) => budget.id === id)[0];
+  return envelope;
+};
+
+allBudgets = new Array(5).fill(0).map(createTempData);
+
+export {
+  createNewBugetEnvelope,
+  getAllFromDatabase,
+  hasAnyBudgets,
+  getEnvelopeById,
+};
