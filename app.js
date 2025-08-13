884d99606b0ce2095d8da25e97dc8e8921e7eb20
mdoliver <mdoliver43@gmail.com>
2025-08-13 09:01:03 +0200

    Found code error when testing with Postman, updated code to reflect fix

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import envelopeRouter from "./server/envelopeApi.js";
 import {
-  getAllFromDatabase,
-  hasAnyBudgets,
-  removeFromDatabaseById,
-  getEnvelopeById,
-  createEnvelope,
-  addToDatabase,
+    getAllFromDatabase,
+    hasAnyBudgets,
+    removeFromDatabaseById,
+    getEnvelopeById,
+    createEnvelope,
+    addToDatabase,
 } from "./public/data.js";
 
 const app = express();
@@ -17,45 +17,48 @@ app.use(bodyParser.json());
 app.use("/envelopes", envelopeRouter);
 
 app.get("/", (req, res, next) => {
-  res.send("<h1>Hello World!</h1>");
+    res.send("<h1>Hello World!</h1>");
 });
 
-envelopeRouter.get("/envelopes", (req, res, next) => {
-  if (!hasAnyBudgets()) {
-    res.send(
-      "No budgets set. Create a new envelope to help track your budgets"
-    );
-  } else {
-    res.status(200).send(getAllFromDatabase());
-  }
+envelopeRouter.get("/", (req, res, next) => {
+    if (!hasAnyBudgets()) {
+        res.send(
+            "No budgets set. Create a new envelope to help track your budgets"
+        );
+    } else {
+        res.status(200).send(getAllFromDatabase());
+    }
 });
 
 envelopeRouter.get("/:id", (req, res, next) => {
-  const envelopeId = Number(req.params.id);
+    const envelopeId = Number(req.params.id);
 
-  if (getEnvelopeById(envelopeId) === null) {
-    res.status(404).send("Can't find the requested envelope");
-  } else {
-    const envelope = getEnvelopeById(envelopeId);
-    res.status(200).send(envelope);
-  }
+    if (getEnvelopeById(envelopeId) === null) {
+        res.status(404).send("Can't find the requested envelope");
+    } else {
+        const envelope = getEnvelopeById(envelopeId);
+        res.status(200).send(envelope);
+    }
 });
 
 envelopeRouter.post("/", (req, res, next) => {
-  const envBody = req.body;
-  const envelope = addToDatabase(createEnvelope(envBody.title, envBody.budget));
-  res.status(201).send(envelope);
+    const envBody = req.body;
+
+    const envelope = addToDatabase(
+        createEnvelope(envBody.title, envBody.budget)
+    );
+    res.status(201).send(envelope);
 });
 
 envelopeRouter.delete("/:id", (req, res, next) => {
-  const id = Number(req.params.id);
-  if (removeFromDatabaseById(id)) {
-    res.status(204).send("Removed envelope successfully");
-  } else {
-    res.status(404).send("Can't find the requested envelope");
-  }
+    const id = Number(req.params.id);
+    if (removeFromDatabaseById(id)) {
+        res.status(204).send("Removed envelope successfully");
+    } else {
+        res.status(404).send("Can't find the requested envelope");
+    }
 });
 
 app.listen(PORT, () => {
-  console.log(`Server started on porrt ${PORT}`);
+    console.log(`Server started on port ${PORT}`);
 });
--- public/data.js
+++ public/data.js
@@ -1,101 +1,101 @@
 let idIterator = 0;
 
 const verifyEnvelopeData = (envelope) => {
-  envelope.title = envelope.title || "";
-  envelope.budget = envelope.budget || 0;
+    envelope.title = envelope.title || "";
+    envelope.budget = envelope.budget || 0;
 
-  if (typeof title !== "string")
-    throw new Error("Title needs to be a string type");
-  if (!isNaN(parseFloat(budget) && isFinite(budget))) {
-    envelope.budget = Number(budget);
-  } else throw new Error("The budget needs to be a number type");
+    if (typeof envelope.title !== "string")
+        throw new Error("Title needs to be a string type");
+    if (!isNaN(parseFloat(envelope.budget) && isFinite(envelope.budget))) {
+        envelope.budget = Number(envelope.budget);
+    } else throw new Error("The budget needs to be a number type");
 
-  return true;
+    return true;
 };
 
 const createEnvelope = (title, budget) => {
-  return {
-    id: ++idIterator,
-    title: title,
-    budget: budget,
-  };
+    return {
+        id: ++idIterator,
+        title: title,
+        budget: budget,
+    };
 };
 
 let allBudgets = [];
 
 let totalBudget = () => {
-  return allBudgets.reduce((accumulator, reducer) => {
-    return (accumulator += reducer.budget);
-  }, 0);
+    return allBudgets.reduce((accumulator, reducer) => {
+        return (accumulator += reducer.budget);
+    }, 0);
 };
 
 const createTempData = () => {
-  let defaultCategories = [
-    "groceries",
-    "fuel",
-    "healthcare",
-    "clothing",
-    "diningOut",
-    "householdItems",
-    "petCare",
-    "family",
-  ];
-  let defaultBudgetValues = [250, 500, 750, 1000, 1500, 2000];
-  const randomCategory = Math.floor(Math.random() * defaultCategories.length);
-  const randomBuget = Math.floor(Math.random() * defaultBudgetValues.length);
-  const envelope = createEnvelope(
-    defaultCategories[randomCategory],
-    defaultBudgetValues[randomBuget]
-  );
-  return envelope;
+    let defaultCategories = [
+        "groceries",
+        "fuel",
+        "healthcare",
+        "clothing",
+        "diningOut",
+        "householdItems",
+        "petCare",
+        "family",
+    ];
+    let defaultBudgetValues = [250, 500, 750, 1000, 1500, 2000];
+    const randomCategory = Math.floor(Math.random() * defaultCategories.length);
+    const randomBuget = Math.floor(Math.random() * defaultBudgetValues.length);
+    const envelope = createEnvelope(
+        defaultCategories[randomCategory],
+        defaultBudgetValues[randomBuget]
+    );
+    return envelope;
 };
 
 const hasAnyBudgets = () => {
-  return allBudgets.length > 0;
+    return allBudgets.length > 0;
 };
 
 const getAllFromDatabase = () => {
-  return allBudgets;
+    return allBudgets;
 };
 
 const addToDatabase = (instance) => {
-  if (verifyEnvelopeData(instance)) {
-    allBudgets.push(instance);
-  }
-  return allBudgets[allBudgets.length - 1];
+    if (verifyEnvelopeData(instance)) {
+        allBudgets.push(instance);
+    }
+    return allBudgets[allBudgets.length - 1];
 };
 
 const removeFromDatabaseById = (id) => {
-  if (isNaN(parseFloat(id)) && !isFinite(id)) {
-    return null;
-  } else {
-    id = Number(id);
-    const index = allBudgets.findIndex((envelope) => envelope.id === id);
-    if (allBudgets[index] !== undefined && index !== -1) {
-      allBudgets.splice(index, 1);
-      return true;
-    } else return false;
-  }
+    if (isNaN(parseFloat(id)) && !isFinite(id)) {
+        return null;
+    } else {
+        id = Number(id);
+        const index = allBudgets.findIndex((envelope) => envelope.id === id);
+        if (allBudgets[index] !== undefined && index !== -1) {
+            allBudgets.splice(index, 1);
+            return true;
+        } else return false;
+    }
 };
 
 const getEnvelopeById = (id) => {
-  if (isNaN(parseFloat(id)) && !isFinite(id)) {
-    return null;
-  } else {
-    id = Number(id);
-    const envelope = allBudgets.find((budget) => budget.id === id);
-    if (envelope !== undefined) return envelope;
-    else return null;
-  }
+    if (isNaN(parseFloat(id)) && !isFinite(id)) {
+        return null;
+    } else {
+        id = Number(id);
+        const envelope = allBudgets.find((budget) => budget.id === id);
+        if (envelope !== undefined) return envelope;
+        else return null;
+    }
 };
 
 allBudgets = new Array(5).fill(0).map(createTempData);
 
 export {
-  createEnvelope,
-  getAllFromDatabase,
-  removeFromDatabaseById,
-  addToDatabase,
-  hasAnyBudgets,
-  getEnvelopeById,
+    createEnvelope,
+    getAllFromDatabase,
+    removeFromDatabaseById,
+    addToDatabase,
+    hasAnyBudgets,
+    getEnvelopeById,
 };
